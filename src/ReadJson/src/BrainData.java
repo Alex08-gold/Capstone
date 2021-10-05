6c37c9ba5b7614da664710c074315d24d84e0f37
Jessica Cooper <cprjes002@cs.uct.ac.za>
2021-10-05 11:01:15 +0000

    Update BrainData.java

--- src/ReadJson/src/BrainData.java
+++ src/ReadJson/src/BrainData.java
@@ -2,4 +2,50 @@ package ReadJson.src;
 
 public class BrainData {
 
+private String id;
+	private String type;
+	private double gain;
+	private double period;
+	private double phaseOffset;
+	
+	public BrainData(String id, double gain) {
+		this.id = id;
+		this.gain = gain;
+		this.type = "Sigmoid";
+		this.period = -1;
+		this.phaseOffset = -1;
+	}
+
+    public BrainData(String id, double gain, double period, double phaseOffset) {
+		this.id = id;
+		this.gain = gain;
+		this.period = period;
+		this.phaseOffset = phaseOffset;
+		this.type = "Oscillator";
+	}
+	
+	public String getID() {
+		return this.id;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+	
+	public double getGain() {
+		return this.gain;
+	}
+	
+	public double getPeriod() {
+		return period;
+	}
+	
+	public double getOffset() {
+		return phaseOffset;
+	}
+	
+	public String toString() {
+		return id+" "+type+" "+gain+" "+period+" "+phaseOffset;
+	}
+
 }
